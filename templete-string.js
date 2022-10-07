aae53b32f45a6594344e6d25116745bb119aa5e3
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-10-07 17:32:58 +0600

    templete string

---
+++ templete-string.js
@@ -0,0 +1,25 @@
+
+// multiline string
+
+const multiline = `this my first line
+
+this my second line
+
+this my third line
+
+this my fourth line
+
+this my fifth line
+
+`;
+
+console.log(multiline);
+
+
+const a = 20;
+const b = 30;
+
+const result = `sum of ${a} and ${b} is: ${a + b}
+
+`;
+console.log(result);
