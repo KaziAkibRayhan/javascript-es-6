1c2de7b2ea6db144a2f711a4fdf2bed87d852dbb
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-10-07 17:01:44 +0600

    js default parameters

---
+++ default-parameters.js
@@ -0,0 +1,9 @@
+function addition(first = 0, second = 0) {
+    return first + second;
+}
+// console.log(addition(23));
+
+function makeFullName(first, last = 'Kazi') {
+    return first + ' ' + last;
+}
+console.log(makeFullName('Akib'));
